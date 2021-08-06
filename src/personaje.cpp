c1d83d4f10d222d58844f8848701e77303def0b0
dennisMSF <87782818+dennisMSF@users.noreply.github.com>
2021-08-05 22:42:16 -0500

    Update personaje.cpp

--- src/personaje.cpp
+++ src/personaje.cpp
@@ -1,41 +1,60 @@
 #include "personaje.h"
-#include <stdlib.h>
-#include<windows.h>
-personaje::personaje():sf::Sprite(){
+#include<iostream>
+personaje::personaje():Sprite(){
     frame=0;
-    row=0;
-    //spritewalk(texturewalk);
-   // spritewalk.setTextureRect(IntRect(71,1,35,50));
-    texturewalk.loadFromFile("img/movimiento2.jpg");
-    this->setTexture(texturewalk);
-    this->setTextureRect(sf::IntRect(40,40,40,40));
-//    this->setTextureRect()
-    this->setPosition(40,40);
+    player1.loadFromFile("img/walk.png");
+    this->setTexture(player1);
+    this->setTextureRect(sf::IntRect(60,60,60,60));
+    this->setPosition(60,60);
 }
 void personaje::mover(){
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
-        this->move(-40, 0);
-        Sleep(150);
-        this->setTextureRect(sf::IntRect(40*frame,120,40,40));
-        frame ++;
+        x-=60;
+        if(x>0&& (y==60 || y==180 || y==300|| y==420 || y==540)){
+            std::cout<<x<<","<<y<<std::endl;
+            this->move(-60, 0);
+            Sleep(150);
+            this->setTextureRect(sf::IntRect(60*frame,180,60,60));
+            frame ++;}
+        else{
+            y=this->getPosition().x;
+        }
     }
     else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
-        this->move(40, 0);
-        Sleep(150);
-        this->setTextureRect(sf::IntRect(40*frame,80,40,40));
-        frame++;
+        x+=60;
+        if(x<840 && (y==60 || y==180 || y==300|| y==420 || y==540)){
+            std::cout<<x<<","<<y<<std::endl;
+            this->move(60, 0);
+            Sleep(150);
+            this->setTextureRect(sf::IntRect(60*frame,120,60,60));
+            frame++;}
+        else{
+            y=this->getPosition().x;
+        }
     }
     else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){
-        this->move(0, -40);
-        Sleep(150);
-       this->setTextureRect(sf::IntRect(40*frame,40,40,40));
-        frame ++;
+        y-=60;
+        if(y>0&& (x==60 || x==180 ||x==300|| x==420 || x==540 || x==660 || x==780) ){
+            std::cout<<x<<","<<y<<std::endl;
+            this->move(0, -60);
+            Sleep(150);
+            this->setTextureRect(sf::IntRect(60*frame,60,60,60));
+            frame ++;}
+        else{
+            y=this->getPosition().y;
+        }
     }
     else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)){
-        this->move(0, 40);
-        Sleep(150);
-        this->setTextureRect(sf::IntRect(40*frame,1,40,40));
-        frame++;
+        y+=60;
+        if(y<600&& (x==60 || x==180 ||x==300|| x==420 || x==540 || x==660 || x==780) ){
+            std::cout<<x<<","<<y<<std::endl;
+            this->move(0, 60);
+            Sleep(150);
+            this->setTextureRect(sf::IntRect(60*frame,0,60,60));
+            frame++;}
+        else{
+            y=this->getPosition().y;
+        }
     }
     if(frame>=8){
         frame=0;
